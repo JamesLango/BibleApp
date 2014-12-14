fee7f1fb1532b3ad92dee7dc412f31b5fc5daff6
Chloe <chloebarnes3@gmail.com>
2014-12-14 21:48:32 +0000

    Word++

---
+++ BibleApplication/src/Word.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+
+
+public class Word {
+	
+	private String wordValue;
+	private int wordCount = 0;
+	private Location location;
+	
+	private ArrayList<Location> locationList;
+	
+	public Word(String word)
+	{	
+		wordValue = word;
+		locationList = new ArrayList<>();
+			
+	}
+	
+	public void updateLocList(Verse v)
+	{
+		//locationList.add(v.getLocation().getLocation().getName(), v.getLocation().getIndex(), v.getIndex());
+	}
+
+	public void incrementWordCount()
+	{
+		wordCount++;
+	}
+	
+	
+	public String getString() {
+		return wordValue;
+	}
+	
+	public Word getWordObject() {
+		return this;
+	}
+	
+	/**
+	 * 
+	 */
+	public String toString()
+	{
+		return wordValue;
+	}
+	
+	public int getCount() {
+		return wordCount;
+	}
+	
+	public ArrayList<Location> getVerses()
+	{
+		return locationList;
+	}
+}
